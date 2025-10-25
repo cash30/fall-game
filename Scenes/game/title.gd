81944da010c5d1014638742656e78724784415a9
Cassius Crum <cashjackc@gmail.com>
2025-10-24 20:31:04 -0400

    add the title screen

--- Scenes/game/title.gd
+++ Scenes/game/title.gd
@@ -1,4 +1,5 @@
-extends Label
+extends Control
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,7 +8,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	text = "Score: "+ str(Globals.score)
-	if Globals.hasGameEnded:
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			Globals.start()
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		Globals.start()
+		
--- scripts/game/Globals.gd
+++ scripts/game/Globals.gd
@@ -2,11 +2,13 @@ extends Node
 
 @export var loseScreen : PackedScene
 @export var main : PackedScene
+@export var titleScreen : PackedScene
 var playerHealth : float = 100.0
  
 var score : int = 0
 var hasGameEnded : bool = false
 var isPlayerSafe : bool = false
+var didStart = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,6 +16,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if !didStart:
+		showTitle()
+
 	if playerHealth <= 0.0 and !hasGameEnded:
 		hasGameEnded = true
 		endGame()
@@ -23,9 +28,15 @@ func _process(_delta: float) -> void:
 func endGame() -> void:
 	get_tree().change_scene_to_packed(loseScreen);
 	
-func restart() -> void:
+func start() -> void:
 	get_tree().change_scene_to_packed(main)
 	playerHealth = 100
 	isPlayerSafe = false
 	hasGameEnded = false
 	score = 0
+	
+func showTitle() -> void:
+	get_tree().change_scene_to_packed(titleScreen)
+	didStart = true
+	
+	
